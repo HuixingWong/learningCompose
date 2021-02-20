03c928b8717fd240df8754a354493de6f27a6da8
HuixingWang <wanghuixing24@gmail.com>
2021-02-20 14:42:20 +0800

    add compose navigation

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.learningcompose.sample.Anim
 import com.example.learningcompose.sample.Counter
+import com.example.learningcompose.sample.Home
 import com.example.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContent {
             LearningComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Content()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        Surface(color = MaterialTheme.colors.background) {
+                            Home(
+                                    anim = {
+                                        navController.navigate("anim")
+                                    },
+                                    counter = {
+                                        navController.navigate("counter")
+                                    }
+                            )
+                        }
+                    }
+                    composable("anim") {
+                        Surface(color = MaterialTheme.colors.background) {
+                            Anim()
+                        }
+                    }
+                    composable("counter") {
+                        Surface(color = MaterialTheme.colors.background) {
+                            Counter()
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/learningcompose/sample/HomePage.kt
@@ -0,0 +1,35 @@
+package com.example.learningcompose.sample
+
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Home(
+        anim: () -> Unit,
+        counter: () -> Unit,
+) {
+    ScrollableColumn(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(Color.Green),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = anim, modifier = Modifier.padding(16.dp)) {
+            Text("Anim")
+        }
+
+        Button(onClick = counter, modifier = Modifier.padding(16.dp)) {
+            Text("counter")
+        }
+    }
+}
