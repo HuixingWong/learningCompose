f5210432cae89f9a610e0393def026dbf026b316
huixing <wanghuixing24@gmail.com>
2021-05-29 00:55:51 +0800

    efffect samplesw

--- app/src/main/java/com/example/learningcompose/sample/Counter.kt
+++ app/src/main/java/com/example/learningcompose/sample/Counter.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -23,6 +20,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.learningcompose.LocalActivity
 import com.example.learningcompose.viewmodel.CounterViewModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun Counter() {
@@ -49,6 +47,16 @@ fun Counter() {
             onDispose {
                 println("😂  disposeEffect dispose")
             }
+        }
+
+        produceState(initialValue = 1) {
+            awaitDispose {
+
+            }
+        }
+
+        derivedStateOf {
+
         }
 
         Text(text = "Hello ${viewModel.amount}!",
@@ -85,4 +93,20 @@ fun Counter() {
 }
 fun showMessage(context: Context, message:String){
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun LandingScreen(onTimeout: () -> Unit) {
+
+    // This will always refer to the latest onTimeout function that
+    // LandingScreen was recomposed with
+    val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+    // Create an effect that matches the lifecycle of LandingScreen.
+    // If LandingScreen recomposes, the delay shouldn't start again.
+    LaunchedEffect(true) {
+        delay(1000)
+        currentOnTimeout()
+    }
+    /* Landing screen content */
 }
